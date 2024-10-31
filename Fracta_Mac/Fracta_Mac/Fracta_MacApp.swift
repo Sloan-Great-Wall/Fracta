cb5ff1211287876be4367ed0006edfc794a9d5bd
sloan <sloan_gw@magipop.xyz>
2024-10-31 15:30:23 +0800

    init macos app

---
+++ Fracta_Mac/Fracta_Mac/Fracta_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fracta_MacApp.swift
+//  Fracta_Mac
+//
+//  Created by Sloan GW on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Fracta_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fracta_Mac/Fracta_MacTests/Fracta_MacTests.swift
@@ -0,0 +1,16 @@
+//
+//  Fracta_MacTests.swift
+//  Fracta_MacTests
+//
+//  Created by Sloan GW on 10/31/24.
+//
+
+import Testing
+
+struct Fracta_MacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
